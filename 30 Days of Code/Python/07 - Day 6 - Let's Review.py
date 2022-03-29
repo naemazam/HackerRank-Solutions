9bdc19fdebfb7ad7635f124e397db1b29feebe4a
Cheatsheets Hub <87407112+CheatSheetsHub@users.noreply.github.com>
2022-03-29 15:56:30 +0800

    Update 07 - Day 6 - Let's Review.py

--- 30 Days of Code/Python/07 - Day 6 - Let's Review.py
+++ 30 Days of Code/Python/07 - Day 6 - Let's Review.py
@@ -7,6 +7,28 @@
 # Max Score: 30
 # Language: Python
 
+
+# Explanation
+
+Test Case 0: S = “Hacker”
+S[0] = “H”
+S[1] = “a”
+S[2] = “c”
+S[3] = “k”
+S[4] = “e”
+S[5] = “r”
+The even indices are 0, 2, and 4, and the odd indices are 1, 3, and 5. We then print a single line of 2 space-separated strings;
+the first string contains the ordered characters from S‘s even indices (Hce), and the second string contains the ordered characters from S‘s odd indices (akr).
+
+Test Case 1: S = “Rank”
+S[0] = “R”
+S[1] = “a”
+S[2] = “n”
+S[3] = “k”
+The even indices are 0, 2 and 4, and the odd indices are 1, 3 and 5. We then print a single line of 2 space-separated strings; the first string contains the ordered characters from S‘s even indices (Rn),
+and the second string contains the ordered characters from S‘s odd indices (ak).
+
+
 # ========================
 #         Solution
 # ========================
