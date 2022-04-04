f51b21f21932e60512116ac973043c7d5c8d0777
Cheatsheets Hub <87407112+CheatSheetsHub@users.noreply.github.com>
2022-04-04 18:11:11 +0800

    Update 20 - Day 19 - Interfaces.py

--- 30 Days of Code/Python/20 - Day 19 - Interfaces.py
+++ 30 Days of Code/Python/20 - Day 19 - Interfaces.py
@@ -17,6 +17,14 @@ class AdvancedArithmetic(object):
 
 class Calculator(AdvancedArithmetic):
     def divisorSum(self, n):
+        pass
+    
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
         sum = 0
 
         for i in range(1, n//2 + 1):
@@ -28,5 +36,4 @@ n = int(input())
 my_calculator = Calculator()
 s = my_calculator.divisorSum(n)
 print("I implemented: " + type(my_calculator).__bases__[0].__name__)
-
 print(s)
